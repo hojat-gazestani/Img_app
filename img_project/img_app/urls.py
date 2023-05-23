e917b94066cee0517fe4fbeec0439a07d6aa56ad
hojat-gazestani <HojatGazestani@gmail.com>
2023-05-23 06:18:00 +0330

    and img amd img/ to img_app urls

--- img_project/img_app/urls.py
+++ img_project/img_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello, name='hello'),
+    path('img', views.img, name='img'),
+    path('img/', views.img, name='img'),
     path('img1/', views.img1, name='img1'),
     path('img2/', views.img2, name='img'),
 ]
