555cf315ac9d1dc6c9591320461395e43e9f7f91
hojat-gazestani <HojatGazestani@gmail.com>
2023-05-23 06:16:27 +0330

    and img function to img_app view

--- img_project/img_app/views.py
+++ img_project/img_app/views.py
@@ -5,8 +5,8 @@ from img_project.context_processors import gunicorn_port
 
 hostname = socket.gethostname()
 
-def hello(request):
-    return HttpResponse(f"Hello form {hostname}, port: { gunicorn_port(request)}")
+def img(request):
+    return HttpResponse(f"Image on {hostname}, port: { gunicorn_port(request)}")
     
 def img1(request):
     return HttpResponse(f"Image1 on {hostname}, port: { gunicorn_port(request)}")
