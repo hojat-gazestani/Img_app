44efbf69e447a55abc99dd28c4b40d513a7fddf8
hojat-gazestani <HojatGazestani@gmail.com>
2023-05-26 13:20:25 +0330

    Added readme file

--- img_project/img_app/urls.py
+++ img_project/img_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.img, name='img'),
     path('img', views.img, name='img'),
-    path('img/', views.img, name='img'),
-    path('img1/', views.img1, name='img1'),
-    path('img2/', views.img2, name='img'),
+    path('img1', views.img1, name='img1'),
+    path('img2', views.img2, name='img'),
 ]
